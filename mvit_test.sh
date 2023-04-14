958f0645505cc9fb59d7227a57ab0e39f5113ad7
CGTpeach <93721759+CGTpeach@users.noreply.github.com>
2023-04-14 13:21:00 +0800

    Create mvit_test.sh

---
+++ mvit_test.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH -p prod
+#SBATCH -t 7-00:00:00
+#SBATCH --cpus-per-task=4
+#SBATCH --gres=gpu:1
+#SBATCH -J TRAIN
+source ~/.bashrc
+conda activate class
+CUDA_VISIBLE_DEVICES=0 python /public/home/chenguotao2021/classification/MobileViT/test.py
